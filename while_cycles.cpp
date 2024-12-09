bf4cec7165a93a82f460413c785ec54efa585fa1
Python arch <py_official@mail.ru>
2024-12-10 00:25:43 +0300

    new file

---
+++ while_cycles.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+
+    // int a = 0;
+    // while (a < 10) {
+    //     cout << "Текст на экране" << endl;
+    //     a++;
+    // }
+
+    int a = 20;
+    do {
+        cout << "Текст на экране" << endl;
+    } while (a < 20);
+}
